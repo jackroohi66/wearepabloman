820dbfa583727a5483b1f517b89edce812ac8dd4
Ludovico Einaudi <160977616+jackroohi66@users.noreply.github.com>
2024-03-01 13:16:55 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
-# wearepabloman
-🎙 Campaign site for Pablo Escobar's election.
+<h1 align="center">
+  <img alt="escobar-wordpress" width="652" src="https://jclerc.github.io/assets/repos/banner/escobar-wordpress.jpg">
+  <br>
+</h1>
+
+<p align="center">
+  <img alt="made for: school" src="https://jclerc.github.io/assets/static/badges/made-for/school.svg">
+  <img alt="language: php" src="https://jclerc.github.io/assets/static/badges/language/php.svg">
+  <img alt="made in: 2016" src="https://jclerc.github.io/assets/static/badges/made-in/2016.svg">
+  <br>
+  <sub>Campaign site for Pablo Escobar's fake election.</sub>
+</p>
+<br>
+
+## Features
+
+- [x] Custom WordPress theme
+- [x] Every field can be edited
+- [x] Custom post types / roles
+- [x] Templates use twig
+
+## Stack used
+
+- WordPress `4.6.1`
+- Timber `1.1`
+- ACF `4.4.11`
+- PHP `7.0` / MySQL `5.6`
+
+## Getting started
+
+#### Requirements
+
+- Apache server with PHP 7+ and MySQL 5.6+
+- `composer` (if you have Homebrew: `brew install composer`)
+- `yarn` (or `npm`)
+- `gulp` (`yarn global add gulp-cli` or `npm i -g gulp-cli`)
+
+#### Installation
+
+```sh
+git clone https://github.com/jclerc/escobar-wordpress.git
+cd escobar-wordpress
+```
+
+Then:
+1. Create a new MySQL database (name: `wpescobar`, charset: `utf8mb4_unicode_ci` and import the file [`wpescobar.sql`](/wpescobar.sql)
+2. Check [`wpescobar/wp-config.php`](/wpescobar/wp-config.php) to ensure database credentials are correct
+3. `cd` to [`wpescobar/wp-content/themes/WP-Escobar`](/wpescobar/wp-content/themes/WP-Escobar), and run `composer install` to fetch dependencies
+4. `cd` to [`wpescobar/wp-content/themes/WP-Escobar/assets`](/wpescobar/wp-content/themes/WP-Escobar/assets), and run `yarn` (or `npm i`) to fetch dependencies
+5. In this last directory, run `gulp` to build assets and watch for changes
+6. Lastly, start the webserver in the [`wpescobar`](/wpescobar) directory!
---
+++ wp-config.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * La configuration de base de votre installation WordPress.
+ *
+ * Ce fichier contient les réglages de configuration suivants : réglages MySQL,
+ * préfixe de table, clés secrètes, langue utilisée, et ABSPATH.
+ * Vous pouvez en savoir plus à leur sujet en allant sur
+ * {@link http://codex.wordpress.org/fr:Modifier_wp-config.php Modifier
+ * wp-config.php}. C’est votre hébergeur qui doit vous donner vos
+ * codes MySQL.
+ *
+ * Ce fichier est utilisé par le script de création de wp-config.php pendant
+ * le processus d’installation. Vous n’avez pas à utiliser le site web, vous
+ * pouvez simplement renommer ce fichier en "wp-config.php" et remplir les
+ * valeurs.
+ *
+ * @package WordPress
+ */
+
+// ** Réglages MySQL - Votre hébergeur doit vous fournir ces informations. ** //
+/** Nom de la base de données de WordPress. */
+define('DB_NAME', 'wpescobar');
+
+/** Utilisateur de la base de données MySQL. */
+define('DB_USER', 'root');
+
+/** Mot de passe de la base de données MySQL. */
+define('DB_PASSWORD', 'root');
+
+/** Adresse de l’hébergement MySQL. */
+define('DB_HOST', 'localhost');
+
+/** Jeu de caractères à utiliser par la base de données lors de la création des tables. */
+define('DB_CHARSET', 'utf8mb4');
+
+/** Type de collation de la base de données.
+  * N’y touchez que si vous savez ce que vous faites.
+  */
+define('DB_COLLATE', '');
+
+/**#@+
+ * Clés uniques d’authentification et salage.
+ *
+ * Remplacez les valeurs par défaut par des phrases uniques !
+ * Vous pouvez générer des phrases aléatoires en utilisant
+ * {@link https://api.wordpress.org/secret-key/1.1/salt/ le service de clefs secrètes de WordPress.org}.
+ * Vous pouvez modifier ces phrases à n’importe quel moment, afin d’invalider tous les cookies existants.
+ * Cela forcera également tous les utilisateurs à se reconnecter.
+ *
+ * @since 2.6.0
+ */
+define('AUTH_KEY',         '9q3d~s53~<UVz:1iHMy<[I2~wABYUp?|q[ioCY`UPC!,&AazAr&Adtv~.q`0(C1z');
+define('SECURE_AUTH_KEY',  'x4~<(o*t=v=!JNM.*!c(!VWrdG_P+K<8bn|Y_LP V1hN]PwPA2j[r@Of/b6E?y+k');
+define('LOGGED_IN_KEY',    'ma5}lx{H~I.~9V2Ho1k+ZxXvM=:f(Ot<M>+9$0rCPV_[KylJ$XF@scxPQ)#=newY');
+define('NONCE_KEY',        'DL;R%xID:4$|NX{*fi<T1E]kM,+$2g$eQXw.UrH,7J#kp]+MDY2:!HIKi0i1L+dY');
+define('AUTH_SALT',        '7<X^@o 4b=Xd~>g/(45r/!@vi+X^^AUjI{|61&-tj-*)>NkwDcM~}5oECK.dZ={/');
+define('SECURE_AUTH_SALT', '&5yrtd,txeK11gP^RAk8I5a&Gnl3BL&(Xk16jJi(/F#K#1Ny25g:6M`5H$_v?; m');
+define('LOGGED_IN_SALT',   '/q.kt%@]kcb[E)$/0D;EV8Gz4wLeK0hF%|g[7L+@VHL`7eDE(MZ%,0L>BT3X.{Ux');
+define('NONCE_SALT',       'n0<m8`%sQ()cFqWy`S6Aci*YGH1p_iM87%<Ez%f,WKJ%9t.UM6Jf[[<)@%J](^Nw');
+/**#@-*/
+
+/**
+ * Préfixe de base de données pour les tables de WordPress.
+ *
+ * Vous pouvez installer plusieurs WordPress sur une seule base de données
+ * si vous leur donnez chacune un préfixe unique.
+ * N’utilisez que des chiffres, des lettres non-accentuées, et des caractères soulignés !
+ */
+$table_prefix  = 'wp_';
+
+/**
+ * Pour les développeurs : le mode déboguage de WordPress.
+ *
+ * En passant la valeur suivante à "true", vous activez l’affichage des
+ * notifications d’erreurs pendant vos essais.
+ * Il est fortemment recommandé que les développeurs d’extensions et
+ * de thèmes se servent de WP_DEBUG dans leur environnement de
+ * développement.
+ *
+ * Pour plus d'information sur les autres constantes qui peuvent être utilisées
+ * pour le déboguage, rendez-vous sur le Codex.
+ *
+ * @link https://codex.wordpress.org/Debugging_in_WordPress
+ */
+define('WP_DEBUG', false);
+
+/* C’est tout, ne touchez pas à ce qui suit ! */
+
+/** Chemin absolu vers le dossier de WordPress. */
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
+
+/** Réglage des variables de WordPress et de ses fichiers inclus. */
+require_once(ABSPATH . 'wp-settings.php');
